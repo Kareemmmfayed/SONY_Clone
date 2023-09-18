14456b3000c2665a2879bc7723e8b4cbee0b9c65
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2023-09-18 20:22:14 +0300

    Finished the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,42 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faBars } from '@fortawesome/free-solid-svg-icons'
 import logo from '../assets/myLogo.jpg'
+import { useMediaQuery } from 'react-responsive';
+
 
 function Header() {
+    const isLargeScreen = useMediaQuery({ query: '(min-width: 640px)' });
+
     return (
         <header>
-            <div className="container">
+            <div className="container col-10">
+                {!isLargeScreen &&
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                }
                 <img src={logo} alt="logo" />
-                <div>
-                    <ul>
-                        <li>Home</li>
-                        <li>Businesses & Products</li>
-                        <li>About Sony Group</li>
-                        <li>Technology</li>
-                        <li>Employees</li>
-                        <li>Sustainability</li>
-                        <li>Design</li>
-                        <li>Investor Relations</li>
-                    </ul>
-                    <ul>
-                        <li>Contact Us</li>
-                        <li>Career | </li>
-                        <li> <FontAwesomeIcon icon={faMagnifyingGlass} /> </li>
-                    </ul>
-                </div>
+                {isLargeScreen && 
+                    <div>
+                        <ul className="chosen">
+                            <li>Home</li>
+                            <li>Businesses & Products</li>
+                            <li>About Sony Group</li>
+                            <li>Technology</li>
+                            <li>Employees</li>
+                            <li>Sustainability</li>
+                            <li>Design</li>
+                            <li>Investor Relations</li>
+                        </ul>
+                        <ul>
+                            <li>Contact Us</li>
+                            <li>Career</li>
+                            <span>|</span>
+                            <li> <FontAwesomeIcon icon={faMagnifyingGlass} /> </li>
+                        </ul>
+                    </div>
+                }
+                {!isLargeScreen &&
+                    <FontAwesomeIcon icon={faBars} />
+                }
             </div>
         </header>
     )
